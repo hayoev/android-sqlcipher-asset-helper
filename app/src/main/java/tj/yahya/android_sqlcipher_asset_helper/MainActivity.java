0a7e09ebfa004cc106e4bbc389b49f9f2640ee3c
yahya <24tech.tj@gmail.com>
2018-10-24 14:22:05 +0500

    Used DemoDatabase

--- app/src/main/java/tj/yahya/android_sqlcipher_asset_helper/MainActivity.java
+++ app/src/main/java/tj/yahya/android_sqlcipher_asset_helper/MainActivity.java
@@ -2,6 +2,9 @@ package tj.yahya.android_sqlcipher_asset_helper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DemoDatabase db = new DemoDatabase(getApplicationContext());
+        ArrayList list = db.getUsers();
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        for (Object l : list) {
+            textView.setText( textView.getText()+ "\n"+ l.toString());
+        }
     }
 }
